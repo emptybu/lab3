58643fb529b5fa5c6e9e85505274d0e26a3baab9
emptybu <130836549+emptybu@users.noreply.github.com>
2024-12-08 12:17:16 +0500

    Create 3.py

---
+++ elementSharafutdinov/3.py
@@ -0,0 +1,24 @@
+class ElementSharafutdinov:
+    def __init__(self, name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @property
+    def number(self):
+        return self.__number
+
+    def dump(self):
+        print(f"Name: {self.name}, Symbol: {self.symbol}, Number: {self.number}")
+
+element = ElementSharafutdinov("manganese", "Mn", 25)
+
+element.dump()
