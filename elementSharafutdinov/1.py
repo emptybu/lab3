46f1b43b709f782888d77f5046cdcddbf90792fc
emptybu <130836549+emptybu@users.noreply.github.com>
2024-12-08 12:16:37 +0500

    Create 1.py

---
+++ elementSharafutdinov/1.py
@@ -0,0 +1,11 @@
+class ElementSharafutdinov:
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+
+element = ElementSharafutdinov("manganese", "Mn", 25)
+
+
+print(f"Name: {element.name}, Symbol: {element.symbol}, Number: {element.number}")
