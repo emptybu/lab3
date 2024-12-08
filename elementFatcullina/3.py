dd55a00e6bde03752bfef3cbff0ed607644ec46a
emptybu <130836549+emptybu@users.noreply.github.com>
2024-12-08 12:15:50 +0500

    Create 3.py

---
+++ elementFatcullina/3.py
@@ -0,0 +1,24 @@
+class ElementFatkullina:
+    def __init__(self, name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @property
+    def number(self):
+        return self.__number
+
+    def dump(self):
+        print(f"Name: {self.name}, Symbol: {self.symbol}, Number: {self.number}")
+
+element = ElementFatkullina("Vanadium", "V", 23)
+
+element.dump()
